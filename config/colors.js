fd06d4846347032a8fa9f330bdc16f404f5546f2
joaogabrielvlf <joaogabrielvlf@gmail.com>
2023-02-10 09:16:39 -0300

    📦 NEW: Create Colors Enum

---
+++ config/colors.js
@@ -0,0 +1,14 @@
+export default {
+    bg:{
+        primary: '#003f87',
+        secondary: '#87cefa',
+        warn: '#FFA500',
+        error: '#f00',
+        
+    },
+
+    text:{
+        secondary: '#000',
+    }
+    
+}
